b9e8ad16009a5ee0f0091d06e85ae87c881a18c0
MarinSenzzy <gwstudiogw48090@gmail.com>
2026-08-15 14:51:27 +0200

    added styles for header and home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,39 @@
-import { Link } from "react-router";
-import css from "./Header.module.css";
+import { Link, useLocation } from 'react-router';
+import css from './Header.module.css';
 
-function Header() {
+export function Header() {
+  const { pathname } = useLocation();
+  const isHome = pathname === '/';
+
   return (
-    <>
-      {/* <header className={css.homeheader}>
-        <div
-          className={`container 
-          
-          `}
-          // ${css.headerwrapper}
-        >
+    <header className={`${css.header} ${isHome ? css.headerHome : css.headerDefault}`}>
+      <div className={css.container}>
+        <div className={`${css.sidehead} ${css.lefthead}`}>
           <Link to="/" className={css.logo}>
             Nanny.Services
           </Link>
-
-          <nav className={css.nav}>
-            <Link to="/">Home</Link>
-            <Link to="/nannies">Nannies</Link>
-          </nav>
         </div>
-      </header> */}
-    </>
+        <div className={`${css.sidehead} ${css.righthead}`}>
+          <nav className={css.nav}>
+            <Link to="/" className={pathname === '/' ? css.activeLink : ''}>
+              Home
+            </Link>
+            <Link to="/nannies" className={pathname === '/nannies' ? css.activeLink : ''}>
+              Nannies
+            </Link>
+          </nav>
+
+          <div className={css.authBtns}>
+            <button type="button" className={css.loginBtn}>
+              Log In
+            </button>
+            <button type="button" className={css.registerBtn}>
+              Registration
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -5,13 +5,27 @@ function Home() {
   return (
     <section className={css.home}>
       <div className={`${css.side} ${css.leftSide}`}>
-        <h1 className={css.title}>Make Life Easier for the Family:</h1>
+        <h1 className={css.title}>
+          Make Life Easier <br /> for the Family:
+        </h1>
         <p className={css.subtitle}>
           Find Babysitters Online for All Occasions
         </p>
         <Link to="/nannies" className={css.getStartedBtn}>
           Get started
-          {/* <span className={css.arrow}>↗</span> */}
+          <svg
+            className={css.btnIcon}
+            width="20"
+            height="15"
+            viewBox="0 0 20 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M18.7341 7.94655C19.1177 7.54927 19.1067 6.91621 18.7094 6.53256L12.2353 0.280632C11.838 -0.103017 11.205 -0.0919665 10.8213 0.305314C10.4377 0.702594 10.4487 1.33566 10.846 1.71931L16.6007 7.27658L11.0434 13.0313C10.6598 13.4286 10.6709 14.0616 11.0681 14.4453C11.4654 14.8289 12.0985 14.8179 12.4821 14.4206L18.7341 7.94655ZM0.0349085 8.56589L18.0322 8.25174L17.9973 6.25205L3.62657e-06 6.56619L0.0349085 8.56589Z"
+              fill="#FBFBFB"
+            />
+          </svg>
         </Link>
       </div>
       <div className={`${css.side} ${css.rightSide}`}>
